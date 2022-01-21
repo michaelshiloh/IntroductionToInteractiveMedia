058d211b7f59afef87952cf9d1fa6799a7508a33
Michael Shiloh <michael@michaelshiloh.com>
2022-01-21 10:36:44 +0400

    Update badBody.js

--- badBody.js
+++ badBody.js
@@ -1,18 +1,1 @@
-function setup() {
-  createCanvas( 400, 1000);
 
-  line(200, 300, 200, 400);
-  line(230, 300, 230, 400);
-
-  // body
-  fill(200, 100, 30);
-  circle(215, 270, 80);
-
-  noStroke();
-  // this muse be the head
-  fill(0, 10, 255);
-  circle(215, 200, 40);
-
-  stroke(0, 100, 20);
-  line(200, 200, 250, 200);
-}
